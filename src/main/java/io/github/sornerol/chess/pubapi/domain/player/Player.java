1552edf6b5e57951b70918105c40fa6b26c01acf
Loren Jamison <loren.jamison@gmail.com>
2021-08-22 21:15:52 -0500

    Add player POJO.

---
+++ src/main/java/io/github/sornerol/chess/pubapi/domain/player/Player.java
@@ -0,0 +1,73 @@
+package io.github.sornerol.chess.pubapi.domain.player;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Player {
+
+    @JsonProperty("@id")
+    @Getter @Setter
+    private String id;
+
+    @JsonProperty("url")
+    @Getter @Setter
+    private String url;
+
+    @JsonProperty("username")
+    @Getter @Setter
+    private String username;
+
+    @JsonProperty("player_id")
+    @Getter @Setter
+    private Integer playerId;
+
+    @JsonProperty("title")
+    @Getter @Setter
+    private String title;
+
+    @JsonProperty("status")
+    @Getter @Setter
+    private String status;
+
+    @JsonProperty("username")
+    @Getter @Setter
+    private String name;
+
+    @JsonProperty("avatar")
+    @Getter @Setter
+    private String avatarUrl;
+
+    @JsonProperty("location")
+    @Getter @Setter
+    private String location;
+
+    @JsonProperty("country")
+    @Getter @Setter
+    private String country;
+
+    @JsonProperty("joined")
+    @Getter @Setter
+    private Long joined;
+
+    @JsonProperty("last_online")
+    @Getter @Setter
+    private Long lastOnline;
+
+    @JsonProperty("followers")
+    @Getter @Setter
+    private Integer followers;
+
+    @JsonProperty("is_streamer")
+    @Getter @Setter
+    private Boolean isStreamer;
+
+    @JsonProperty("twitch_url")
+    @Getter @Setter
+    private String twitchUrl;
+
+    @JsonProperty("fide")
+    @Getter @Setter
+    private Integer fideRating;
+
+}
