751d2d050dd0bfdc0288dbc09a4fc043062d00e2
Loren Jamison <loren.jamison@gmail.com>
2021-08-28 23:40:44 -0500

    Implemented StreamersClient.

---
+++ src/main/java/io/github/sornerol/chess/pubapi/domain/streamers/StreamersList.java
@@ -0,0 +1,16 @@
+package io.github.sornerol.chess.pubapi.domain.streamers;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@ToString
+@Getter
+@Setter
+public class StreamersList {
+    @JsonProperty("streamers")
+    private List<Streamer> streamers;
+}
