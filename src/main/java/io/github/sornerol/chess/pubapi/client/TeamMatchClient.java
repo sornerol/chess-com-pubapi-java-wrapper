7fb122c236bb2f341ae2bb93cdd4d3128743c5fa
Loren Jamison <loren.jamison@gmail.com>
2021-09-04 00:21:27 -0500

    Implement methods to get Live and Daily Team match boards.

--- src/main/java/io/github/sornerol/chess/pubapi/client/TeamMatchClient.java
+++ src/main/java/io/github/sornerol/chess/pubapi/client/TeamMatchClient.java
@@ -1,6 +1,7 @@
 package io.github.sornerol.chess.pubapi.client;
 
 import io.github.sornerol.chess.pubapi.domain.match.TeamMatch;
+import io.github.sornerol.chess.pubapi.domain.match.TeamMatchBoard;
 import io.github.sornerol.chess.pubapi.exception.ChessComPubApiException;
 
 import java.io.IOException;
@@ -16,14 +17,30 @@ public class TeamMatchClient extends PubApiClientBase {
         return getRequest(url, TeamMatch.class);
     }
 
-    public TeamMatch getDailyTeamMatchByUrlId(Integer urlId) throws IOException, ChessComPubApiException {
-        String endpoint = String.format("%s/%d", ENDPOINT_BASE, urlId);
+    public TeamMatch getDailyTeamMatchById(Integer id) throws IOException, ChessComPubApiException {
+        String endpoint = String.format("%s/%d", ENDPOINT_BASE, id);
         return getRequest(endpoint, TeamMatch.class);
     }
 
-    public TeamMatch getLiveTeamMatchByUrlId(Integer urlId) throws IOException, ChessComPubApiException {
-        String endpoint = String.format("%s/live/%d", ENDPOINT_BASE, urlId);
+    public TeamMatch getLiveTeamMatchById(Integer id) throws IOException, ChessComPubApiException {
+        String endpoint = String.format("%s/live/%d", ENDPOINT_BASE, id);
         return getRequest(endpoint, TeamMatch.class);
     }
 
+    public TeamMatchBoard getTeamMatchBoardFromApiUrl(String url) throws IOException, ChessComPubApiException {
+        if (!url.startsWith(ENDPOINT_BASE)) {
+            throw new ChessComPubApiException("URL is not a valid team match board API URL");
+        }
+        return getRequest(url, TeamMatchBoard.class);
+    }
+
+    public TeamMatchBoard getDailyTeamMatchBoard(Integer teamMatchId, Integer boardId) throws IOException, ChessComPubApiException {
+        String endpoint = String.format("%s/%d/%d", ENDPOINT_BASE, teamMatchId, boardId);
+        return getRequest(endpoint, TeamMatchBoard.class);
+    }
+
+    public TeamMatchBoard getLiveTeamMatchBoard(Integer teamMatchId, Integer boardId) throws IOException, ChessComPubApiException {
+        String endpoint = String.format("%s/live/%d/%d", ENDPOINT_BASE, teamMatchId, boardId);
+        return getRequest(endpoint, TeamMatchBoard.class);
+    }
 }
---
+++ src/main/java/io/github/sornerol/chess/pubapi/domain/match/TeamMatchBoard.java
@@ -0,0 +1,21 @@
+package io.github.sornerol.chess.pubapi.domain.match;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.github.sornerol.chess.pubapi.domain.game.ArchiveGame;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@ToString
+public class TeamMatchBoard {
+    @JsonProperty("board_scores")
+    Map<String, Integer> boardScores;
+
+    @JsonProperty("games")
+    List<ArchiveGame> games;
+}
