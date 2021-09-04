8b0325fbc6b73753cf1619c65a38067eff36f159
Loren Jamison <loren.jamison@gmail.com>
2021-09-04 00:52:46 -0500

    Implement CountryClient.

---
+++ src/main/java/io/github/sornerol/chess/pubapi/domain/club/ClubList.java
@@ -0,0 +1,16 @@
+package io.github.sornerol.chess.pubapi.domain.club;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class ClubList {
+    @JsonProperty("clubs")
+    List<String> clubsApiUrls;
+}
