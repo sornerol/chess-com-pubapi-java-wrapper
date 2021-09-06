c06f0b5a3dbbfaa44ed371c44553f99119f46eb5
Loren Jamison <loren.jamison@gmail.com>
2021-09-05 23:47:12 -0500

    Javadoc for Country domain object.

--- src/main/java/com/github/sornerol/chess/pubapi/domain/country/Country.java
+++ src/main/java/com/github/sornerol/chess/pubapi/domain/country/Country.java
@@ -5,16 +5,28 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * Contains information about a country recognized by the Chess.com platform
+ */
 @Getter
 @Setter
 @ToString
 public class Country {
+    /**
+     * Self-referencing PubAPI URL for this country
+     */
     @JsonProperty("@id")
     private String id;
 
+    /**
+     * Country's name
+     */
     @JsonProperty("name")
     private String name;
 
+    /**
+     * Country's ISO 3166 code
+     */
     @JsonProperty("code")
     private String code;
 }
