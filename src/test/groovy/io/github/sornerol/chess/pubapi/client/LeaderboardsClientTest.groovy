9ae9d12fa70fd6d1179b94c6bd1e533ecde4accf
Loren Jamison <loren@lorenjamison.com>
2024-01-21 18:04:32 -0600

    Add LeaderboardsClient tests.

---
+++ src/test/groovy/io/github/sornerol/chess/pubapi/client/LeaderboardsClientTest.groovy
@@ -0,0 +1,23 @@
+package io.github.sornerol.chess.pubapi.client
+
+import io.github.sornerol.chess.pubapi.domain.leaderboards.Leaderboards
+import spock.lang.Specification
+
+
+class LeaderboardsClientTest extends Specification {
+    private LeaderboardsClient sut
+
+    def setup() {
+        sut = new LeaderboardsClient()
+        sut.userAgent = 'chesscom-pubapi-wrapper unit tests; username: lrj825; github.com/sornerol/chess-com-pubapi-java-wrapper'
+    }
+
+    def "Get leaderboards"() {
+        when:
+        Leaderboards result = sut.getLeaderboards()
+
+        then:
+        noExceptionThrown()
+        result
+    }
+}
